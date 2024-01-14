f8a4f1a71df7276f1ca48958639398b69a8f15df
Konrad Badzioch <konradbadzioch6@gmail.com>
2024-01-14 11:45:53 +0100

    Add test coverage raport generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,17 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+
+  SimpleCov.start 'rails' do
+    enable_coverage :branch
+  end
+
+  SimpleCov.minimum_coverage line: 95, branch: 90
+end
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
